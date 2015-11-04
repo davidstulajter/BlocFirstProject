b118f96bd647671dd8de39fbc6977900a4c0298c
David Stulajter <davidstulajter@gmail.com>
2015-11-03 20:32:15 -0600

    Devise installed+basic configuration completed

--- bloc-first-project/app/controllers/application_controller.rb
+++ bloc-first-project/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # nothing here yet
+  before_action :authenticate_user!
+  # nothing here yet 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
